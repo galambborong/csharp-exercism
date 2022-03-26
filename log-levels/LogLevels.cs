d07092d98fa911a5511607c7af2fdd4b275dccbb
Peter Keenan <keenan@posteo.net>
2022-03-26 19:37:47 +0000

    Finish LogLevels and refactor

--- log-levels/LogLevels.cs
+++ log-levels/LogLevels.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Linq;
 
 static class LogLine
 {
@@ -6,16 +6,32 @@ static class LogLine
     {
         var logLevelEndIndex = logLine.IndexOf(':') + 2;
 
-        return logLine.Substring(logLevelEndIndex);
+        return logLine.Substring(logLevelEndIndex).Trim();
     }
 
     public static string LogLevel(string logLine)
     {
-        throw new NotImplementedException("Please implement the (static) LogLine.LogLevel() method");
+        var logLevelEndIndex = logLine.IndexOf(':');
+        var rawLogLevel = logLine[..logLevelEndIndex];
+
+        var formattedLogLevel = string.Concat(rawLogLevel.Where(char.IsLetter)).ToLower();
+
+        return formattedLogLevel;
     }
 
     public static string Reformat(string logLine)
     {
-        throw new NotImplementedException("Please implement the (static) LogLine.Reformat() method");
+        var message = Message(logLine).CapitaliseFirstCharacter();
+        var logLevel = LogLevel(logLine);
+
+        return $"{message} ({logLevel})";
+    }
+
+    private static string CapitaliseFirstCharacter(this string str)
+    {
+        var firstChar = str[0];
+        var remainingChars = str[1..];
+
+        return $"{firstChar}{remainingChars}";
     }
 }
