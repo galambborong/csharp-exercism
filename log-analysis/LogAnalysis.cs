2794b2c4558b8e377b9563edb7b7b02787d49214
Peter Keenan <peter.keenan@sage.com>
2022-03-28 12:04:10 +0100

    SubstringAfter extension method

--- log-analysis/LogAnalysis.cs
+++ log-analysis/LogAnalysis.cs
@@ -2,11 +2,23 @@ using System;
 
 public static class LogAnalysis 
 {
-    // TODO: define the 'SubstringAfter()' extension method on the `string` type
+    public static string SubstringAfter(this string logString, string subString)
+    {
+        if (logString is null)
+            throw new ArgumentNullException(nameof(logString));
+
+        var patternLength = subString.Length;
+        var startIndex = logString.IndexOf(subString, StringComparison.Ordinal) + patternLength;
+
+        return logString.Substring(startIndex);
+    }
 
     // TODO: define the 'SubstringBetween()' extension method on the `string` type
-    
+    public static string SubstringBetween(this string logString, string first, string last) => "";
+
     // TODO: define the 'Message()' extension method on the `string` type
+    public static string Message(this string logString) => "";
 
     // TODO: define the 'LogLevel()' extension method on the `string` type
+    public static string LogLevel(this string logString) => "";
 }
