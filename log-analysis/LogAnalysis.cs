c7efead4f80d40a96d4f414b06b1a7f36c1f834b
Peter Keenan <peter.keenan@sage.com>
2022-03-29 07:54:28 +0100

    Second exercise of LogAnalysis first solution

--- log-analysis/LogAnalysis.cs
+++ log-analysis/LogAnalysis.cs
@@ -4,17 +4,37 @@ public static class LogAnalysis
 {
     public static string SubstringAfter(this string logString, string subString)
     {
-        if (logString is null)
+        if (string.IsNullOrEmpty(logString))
             throw new ArgumentNullException(nameof(logString));
 
+        if (string.IsNullOrEmpty(subString))
+            throw new ArgumentNullException(nameof(subString));
+        
         var patternLength = subString.Length;
         var startIndex = logString.IndexOf(subString, StringComparison.Ordinal) + patternLength;
 
         return logString.Substring(startIndex);
     }
 
-    // TODO: define the 'SubstringBetween()' extension method on the `string` type
-    public static string SubstringBetween(this string logString, string first, string last) => "";
+    public static string SubstringBetween(this string logString, string first, string last)
+    {
+        if (string.IsNullOrEmpty(logString))
+            throw new ArgumentNullException(nameof(logString));
+        
+        if (string.IsNullOrEmpty(first))
+            throw new ArgumentNullException(nameof(first));
+
+        if (string.IsNullOrEmpty(last))
+            throw new ArgumentNullException(nameof(last));
+
+        var firstIndex = logString.IndexOf(first, StringComparison.Ordinal) + first.Length;
+
+        var mid = logString.Substring(firstIndex);
+        
+        var lastIndex = mid.IndexOf(last, StringComparison.Ordinal);
+
+        return mid.Substring(0, lastIndex);
+    }
 
     // TODO: define the 'Message()' extension method on the `string` type
     public static string Message(this string logString) => "";
