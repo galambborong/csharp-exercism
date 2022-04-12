932d99f440aa63b8fd2fedb36b0cd736522ad63f
Peter Keenan <peter.keenan@sage.com>
2022-04-12 09:08:57 +0100

    First task passing

--- cars-assemble/CarsAssemble.cs
+++ cars-assemble/CarsAssemble.cs
@@ -2,10 +2,15 @@ using System;
 
 static class AssemblyLine
 {
-    public static double SuccessRate(int speed)
+    public static double SuccessRate(int speed) => speed switch
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.SuccessRate() method");
-    }
+        0 => 0.0,
+        < 5 => 1.0,
+        < 9 => 0.9,
+        9 => 0.8,
+        10 => 0.77,
+        _ => throw new ArgumentOutOfRangeException(nameof(speed), speed, null)
+    };
     
     public static double ProductionRatePerHour(int speed)
     {
