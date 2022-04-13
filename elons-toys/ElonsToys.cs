2f55ee80affbfec1c30d48ef053c8f3acaafc5bd
Peter Keenan <peter.keenan@sage.com>
2022-04-13 14:46:22 +0100

    General refactor of names

--- elons-toys/ElonsToys.cs
+++ elons-toys/ElonsToys.cs
@@ -1,20 +1,29 @@
 class RemoteControlCar
 {
-    private int TotalDistanceDriven { get; set; } = 0;
-    private int BatteryLifeRemaining { get; set; } = 100;
+    private int TotalDistanceDriven { get; set; }
+    private int BatteryRemaining { get; set; } = 100;
     public static RemoteControlCar Buy() => new RemoteControlCar();
 
     public string DistanceDisplay() => $"Driven {TotalDistanceDriven} meters";
 
-    public string BatteryDisplay() => $"Battery at {BatteryLifeRemaining}%";
+    public string BatteryDisplay()
+    {
+        if (BatteryRemaining == 0)
+            return "Battery empty";
+
+        return $"Battery at {BatteryRemaining}%";
+    }
 
     public void Drive()
     {
-        UpdateTotalDistance();
-        UpdateBatteryLife();
+        if (BatteryRemaining <= 0)
+            return;
+
+        IncrementDistance();
+        DecrementBatteryLife();
     }
 
-    private void UpdateBatteryLife() => BatteryLifeRemaining -= 1;
+    private void DecrementBatteryLife() => BatteryRemaining -= 1;
 
-    private void UpdateTotalDistance() => TotalDistanceDriven += 20;
+    private void IncrementDistance() => TotalDistanceDriven += 20;
 }
