f4ff7ce6481c91605a218ebd7a2c2ff448f4c0b7
Peter Keenan <peter.keenan@sage.com>
2022-04-14 11:08:46 +0100

    Initial TimFromMarketing solution

--- tim-from-marketing/TimFromMarketing.cs
+++ tim-from-marketing/TimFromMarketing.cs
@@ -2,8 +2,24 @@ using System;
 
 static class Badge
 {
+    private const string Owner = "OWNER";
+
     public static string Print(int? id, string name, string? department)
     {
-        throw new NotImplementedException("Please implement the (static) Badge.Print() method");
+        if (string.IsNullOrEmpty(name))
+            throw new ArgumentNullException(nameof(name));
+
+        if (id is null)
+        {
+            if (string.IsNullOrEmpty(department))
+                return $"{name} - {Owner}";
+
+            return $"{name} - {department.ToUpper()}";
+        }
+
+        if (string.IsNullOrEmpty(department))
+            return $"[{id}] - {name} - {Owner}";
+
+        return $"[{id}] - {name} - {department.ToUpper()}";
     }
 }
