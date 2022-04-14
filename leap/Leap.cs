045f186067724d64e1ffa1fe596935e2ef4beb66
Peter Keenan <peter.keenan@sage.com>
2022-04-14 14:48:21 +0100

    Solution for Leap

--- leap/Leap.cs
+++ leap/Leap.cs
@@ -1,9 +1,11 @@
-using System;
-
 public static class Leap
 {
-    public static bool IsLeapYear(int year)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static bool IsLeapYear(int year) =>
+        (year % 4, year % 100, year % 400) switch
+        {
+            (0, 0, 0) => true,
+            (0, 0, _) => false,
+            (0, _, _) => true,
+            _ => false
+        };
 }
