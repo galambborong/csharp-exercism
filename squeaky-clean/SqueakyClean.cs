1edadb956dd2e3574bde0f151d2f447e0ca0c69e
Peter Keenan <peter.keenan@sage.com>
2022-04-19 11:12:01 +0100

    WIP on squeeky clean

--- squeaky-clean/SqueakyClean.cs
+++ squeaky-clean/SqueakyClean.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Linq;
 
 public static class Identifier
 {
     public static string Clean(string identifier)
     {
-        throw new NotImplementedException($"Please implement the (static) Identifier.Clean() method");
+        var chars = identifier.ToCharArray();
+        if (!chars.Any(char.IsLetter))
+            return "";
+        
+        string handledIdentifer;
+
+        if (chars.Any(char.IsControl))
+        {
+            var index = identifier.IndexOf('\0');
+
+            var newIdentifier = identifier.Insert(index, "CTRL");
+
+            handledIdentifer = newIdentifier.Remove(index + 4, 1);
+
+            return handledIdentifer;
+        }
+
+        handledIdentifer = identifier.Replace(' ', '_');
+
+        return handledIdentifer;
     }
 }
