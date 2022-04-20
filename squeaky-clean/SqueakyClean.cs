8a7b3a1bc7bd0ef27c5d925dad96acfb805de254
Peter Keenan <peter.keenan@sage.com>
2022-04-20 09:39:55 +0100

    WIP: Clean approach

--- squeaky-clean/SqueakyClean.cs
+++ squeaky-clean/SqueakyClean.cs
@@ -2,42 +2,27 @@ using System.Linq;
 
 public static class Identifier
 {
+    private const char ControlChar = '\0';
+    private const string ControlSubstitution = "CTRL";
     public static string Clean(string identifier)
     {
-        var chars = identifier.ToCharArray();
-        if (!chars.Any(char.IsLetter))
-            return "";
+        return identifier.HandleControlChar().HandleSpaces();
+    }
+
+    private static string HandleControlChar(this string str)
+    {
+        var charArr = str.ToCharArray();
+        if (!charArr.Any(char.IsControl))
+            return str;
         
-        string handledIdentifer;
+        var index = str.IndexOf(ControlChar);
+        var newStr = str.Insert(index, ControlSubstitution);
 
-        if (chars.Any(char.IsControl))
-        {
-            var index = identifier.IndexOf('\0');
+        return newStr.Remove(index + 4, 1);
+    }
 
-            var newIdentifier = identifier.Insert(index, "CTRL");
-
-            handledIdentifer = newIdentifier.Remove(index + 4, 1);
-
-            return handledIdentifer;
-        }
-
-        if (chars.Any(c => c == '-'))
-        {
-            for (var i = 0; i < chars.Length; i++)
-            {
-                if (chars[i] == '-')
-                {
-                    chars[i + 1] = char.ToUpperInvariant(chars[i + 1]);
-                }
-            }
-
-            var newChars = chars.Where(c => c != '-').ToArray();
-
-            return new string(newChars);
-        }
-
-        handledIdentifer = identifier.Replace(' ', '_');
-
-        return handledIdentifer;
+    private static string HandleSpaces(this string str)
+    {
+        return str.Replace(' ', '_');
     }
 }
