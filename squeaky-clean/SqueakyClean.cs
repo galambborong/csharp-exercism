827b91dd18da89d0cfe42729e5a56051e0ecdce4
Peter Keenan <peter.keenan@sage.com>
2022-04-20 10:09:21 +0100

    Add ConvertToCamel

--- squeaky-clean/SqueakyClean.cs
+++ squeaky-clean/SqueakyClean.cs
@@ -6,7 +6,38 @@ public static class Identifier
     private const string ControlSubstitution = "CTRL";
     public static string Clean(string identifier)
     {
-        return identifier.HandleControlChar().HandleSpaces();
+        return identifier.ConvertToCamel().CheckForLetters().HandleControlChar().HandleSpaces();
+    }
+
+    private static string ConvertToCamel(this string str)
+    {
+        var charArr = str.ToCharArray();
+
+        if (charArr.All(c => c != '-')) return str;
+        {
+            for (var i = 0; i < charArr.Length; i++)
+            {
+                if (charArr[i] == '-')
+                {
+                    charArr[i + 1] = char.ToUpperInvariant(charArr[i + 1]);
+                }
+            }
+
+            var newChars = charArr.Where(c => c != '-').ToArray();
+
+            return new string(newChars);
+        }
+
+    }
+
+    private static string CheckForLetters(this string str)
+    {
+        var charArr = str.ToCharArray();
+
+        if (!charArr.Any(char.IsLetter))
+            return "";
+
+        return str;
     }
 
     private static string HandleControlChar(this string str)
